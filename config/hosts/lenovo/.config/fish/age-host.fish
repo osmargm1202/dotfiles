2a5393f84cb35a0ecfc0976b640fd18054875eb0
osmar <osmargm1202@gmail.com>
2026-05-21 09:37:57 +0000

    feat(fish): add lenovo dot aliases

--- config/hosts/lenovo/.config/fish/age-host.fish
+++ config/hosts/lenovo/.config/fish/age-host.fish
@@ -6,3 +6,7 @@ set -gx CHEZMOI_AGE_IDENTITY "$AGE_KEY_FILE"
 set -gx DOT_PRIVATE_ENV_PLAIN "$HOME/.config/fish/private-env.fish"
 set -gx DOT_PRIVATE_ENV_ENCRYPTED "$HOME/.config/fish/private-env.fish.age"
 set -gx DOTFILES_REPO "$HOME/Hobby/dotfiles"
+
+if test -f ~/.config/fish/host-lenovo.fish
+    source ~/.config/fish/host-lenovo.fish
+end
---
+++ config/hosts/lenovo/.config/fish/host-lenovo.fish
@@ -0,0 +1,3 @@
+# Lenovo-specific shortcuts.
+alias dotd='dot diff --host lenovo'
+alias dots='dot sync --host lenovo'
