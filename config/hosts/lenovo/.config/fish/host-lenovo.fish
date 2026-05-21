ec96509c726dde6161463128170d32088e9393d8
osmar <osmargm1202@gmail.com>
2026-05-21 18:48:07 +0000

    chore: commit pending local changes

--- config/hosts/lenovo/.config/fish/host-lenovo.fish
+++ config/hosts/lenovo/.config/fish/host-lenovo.fish
@@ -1,3 +1,3 @@
 # Lenovo-specific shortcuts.
-alias dotd='dot diff --host lenovo'
-alias dots='dot sync --host lenovo'
+alias dotd='orgm-dot diff --host lenovo'
+alias dots='orgm-dot sync --host lenovo'
