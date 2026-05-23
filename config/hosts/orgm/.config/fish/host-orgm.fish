5f76429c5fa1229f42dd45ecbdbbcc9c52ee9894
osmar <osmargm1202@gmail.com>
2026-05-23 07:50:49 +0000

    chore(fish): add orgm os aliases

--- config/hosts/orgm/.config/fish/host-orgm.fish
+++ config/hosts/orgm/.config/fish/host-orgm.fish
@@ -1,3 +1,10 @@
 # ORGM-specific shortcuts.
 alias dotd='orgm-dot diff --host orgm'
 alias dots='orgm-dot sync --host orgm'
+
+# NixOS rebuild shortcuts.
+alias os='nh os switch /home/osmarg/Hobby/dotfiles/ --hostname orgm-hyprland'
+alias osb='nh os build /home/osmarg/Hobby/dotfiles/ --hostname orgm-hyprland'
+alias oss='nh os switch /home/osmarg/Hobby/dotfiles/ --hostname orgm-hyprland'
+alias ossu='nh os switch /home/osmarg/Hobby/dotfiles/ --hostname orgm-hyprland --update'
+alias osbu='nh os build /home/osmarg/Hobby/dotfiles/ --hostname orgm-hyprland --update'
