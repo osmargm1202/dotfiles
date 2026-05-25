ab314e242441f69d3b739b33c4be04aed827df3b
osmar <osmargm1202@gmail.com>
2026-05-25 19:57:48 +0000

    fix(theme): allow local theme settings

--- nixos/profiles/hyprland.nix
+++ nixos/profiles/hyprland.nix
@@ -122,11 +122,6 @@ in
     QT_QPA_PLATFORM = "wayland";
     QT_QPA_PLATFORMTHEME = "qt5ct";
     QT_QPA_PLATFORMTHEME_QT6 = "qt6ct";
-    QT_STYLE_OVERRIDE = "kvantum";
-    GTK_THEME = "catppuccin-macchiato-teal-standard";
-    XCURSOR_THEME = "Catppuccin-Macchiato-Teal-Cursors";
-    XCURSOR_SIZE = "36";
-    HYPRCURSOR_SIZE = "36";
     ELECTRON_OZONE_PLATFORM_HINT = "auto";
     GDK_BACKEND = "wayland,x11";
     SDL_VIDEODRIVER = "wayland";
