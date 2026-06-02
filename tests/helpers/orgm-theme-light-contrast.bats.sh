551c6039402987575bdad3813219d5811ccfcfcd
osmar <osmargm1202@gmail.com>
2026-06-02 14:41:17 +0000

    fix(theme): use proportional UI fonts

--- tests/helpers/orgm-theme-light-contrast.bats.sh
+++ tests/helpers/orgm-theme-light-contrast.bats.sh
@@ -10,7 +10,7 @@ mkdir -p "$TMP/config/orgm-theme/themes" "$TMP/state" "$TMP/runtime" "$TMP/bin"
 cp "$ROOT/config/shared/.config/orgm-theme/themes/orgm-light.env" "$TMP/config/orgm-theme/themes/orgm-light.env"
 cp "$ROOT/config/shared/.config/orgm-theme/themes/orgm-dark.env" "$TMP/config/orgm-theme/themes/orgm-dark.env"
 
-for bin in hyprctl kitty swaync-client nautilus systemctl waybar-watch gsettings orgm-wallpaper; do
+for bin in hyprctl kitty swaync-client nautilus systemctl waybar-watch orgm-wallpaper; do
   cat >"$TMP/bin/$bin" <<'SH'
 #!/usr/bin/env bash
 exit 0
@@ -18,15 +18,27 @@ SH
   chmod +x "$TMP/bin/$bin"
 done
 
+cat >"$TMP/bin/gsettings" <<'SH'
+#!/usr/bin/env bash
+echo "gsettings $*" >>"$GSETTINGS_LOG"
+SH
+chmod +x "$TMP/bin/gsettings"
+: >"$TMP/gsettings.log"
+
 HOME="$TMP/home" \
 XDG_CONFIG_HOME="$TMP/config" \
 XDG_STATE_HOME="$TMP/state" \
 XDG_RUNTIME_DIR="$TMP/runtime" \
 PATH="$TMP/bin:$PATH" \
+GSETTINGS_LOG="$TMP/gsettings.log" \
 "$SCRIPT" apply orgm-light >/tmp/orgm-theme-light-contrast.out
 
 WAYBAR="$TMP/config/waybar-hypr/orgm-current.css"
 GTK="$TMP/config/gtk-4.0/gtk.css"
+GTK3_SETTINGS="$TMP/config/gtk-3.0/settings.ini"
+GTK4_SETTINGS="$TMP/config/gtk-4.0/settings.ini"
+QT5="$TMP/config/qt5ct/qt5ct.conf"
+QT6="$TMP/config/qt6ct/qt6ct.conf"
 KITTY="$TMP/config/kitty/current-theme.conf"
 
 assert_contains() {
@@ -56,6 +68,15 @@ assert_contains "$TMP/config/swaync/orgm-current.css" '@define-color base #fffff
 assert_contains "$TMP/config/swaync/orgm-current.css" '@define-color surface #e5e7ebff;'
 assert_contains "$GTK" '@define-color window_fg_color #111827;'
 assert_contains "$GTK" '@define-color accent_color #0057d9;'
+assert_contains "$GTK3_SETTINGS" 'gtk-font-name=Inter 11'
+assert_contains "$GTK4_SETTINGS" 'gtk-font-name=Inter 11'
+assert_contains "$QT5" 'general="Inter,11,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'
+assert_contains "$QT5" 'fixed="JetBrains Mono,11,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'
+assert_contains "$QT6" 'general="Inter,11,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'
+assert_contains "$QT6" 'fixed="JetBrains Mono,11,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'
+assert_contains "$TMP/gsettings.log" 'gsettings set org.gnome.desktop.interface font-name Inter 11'
+assert_contains "$TMP/gsettings.log" 'gsettings set org.gnome.desktop.interface document-font-name Inter 11'
+assert_contains "$TMP/gsettings.log" 'gsettings set org.gnome.desktop.interface monospace-font-name JetBrains Mono 11'
 assert_contains "$KITTY" 'background_opacity 1.0'
 
 HYPR_RULES=$(ROOT="$ROOT" XDG_STATE_HOME="$TMP/state" HOME="$TMP/home" lua - <<'LUA'
