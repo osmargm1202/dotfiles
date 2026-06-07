303c789590f3e572c4a1dc8c89c792af356c6302
osmar <osmargm1202@gmail.com>
2026-06-07 17:33:05 +0000

    fix(waybar): start hypr profile

--- config/shared/.config/fish/functions/autostart.fish
+++ config/shared/.config/fish/functions/autostart.fish
@@ -1,3 +1,3 @@
 function autostart
-    pypr & hyprpaper & hypridle & waybar & poweralertd -s & wl-paste --type text --watch cliphist store & wl-paste --type image --watch cliphist store & wl-clip-persist --clipboard regular & avizo-service & systemctl --user start psi-notify
+    pypr & hyprpaper & hypridle & waybar-watch ~/.config/waybar-hypr & poweralertd -s & wl-paste --type text --watch cliphist store & wl-paste --type image --watch cliphist store & wl-clip-persist --clipboard regular & avizo-service & systemctl --user start psi-notify
 end
