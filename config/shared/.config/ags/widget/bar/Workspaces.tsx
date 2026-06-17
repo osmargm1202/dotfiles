9544f1373bf79c2020c8b09ce4272af58fac4fa5
osmar <osmargm1202@gmail.com>
2026-06-17 01:25:15 +0000

    fix(ags): add @subtext0 to placeholder colors, add key to workspace map

--- config/shared/.config/ags/widget/bar/Workspaces.tsx
+++ config/shared/.config/ags/widget/bar/Workspaces.tsx
@@ -32,7 +32,7 @@ export default function Workspaces() {
         ⊞
       </button>
       {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(id => (
-        <WorkspaceButton id={id} />
+        <WorkspaceButton key={id} id={id} />
       ))}
     </box>
   )
